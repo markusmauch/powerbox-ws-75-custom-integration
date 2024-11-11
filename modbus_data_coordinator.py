57685ecc176bc3207636306d0e206216cc48ed6e
Markus Mauch <markus.mauch@gmail.com>
2024-11-11 15:31:14 +0100

    Imporove polling

--- modbus_data_coordinator.py
+++ modbus_data_coordinator.py
@@ -1,57 +1,54 @@
-from datetime import timedelta
 import logging
+import asyncio
+from datetime import timedelta
 from .const import MODBUS_REGISTERS
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 from pymodbus.client.tcp import ModbusTcpClient as ModbusClient
-
-_LOGGER = logging.getLogger(__name__)
 
 class ModbusDataCoordinator(DataUpdateCoordinator):
     def __init__(self, hass, modbus_client: ModbusClient):
         super().__init__(
             hass,
-            _LOGGER,
+            logging.getLogger(__name__),
             name="Modbus Coordinator",
-            update_interval=timedelta(seconds=5),
+            update_interval=timedelta(seconds=1),
         )
-        self._update_cache = {}
         self._modbus_client = modbus_client
 
+        self._data = {}
+        self._current_register = -1
+        self._update_list = {}
+
+    def write(self, unique_id: str, value: int):
+        self._update_list[unique_id] = value
+
     async def _async_update_data(self):
-        data = {}
-        update_cache = self._update_cache.copy()
-        processed_updates = self._process_updates()
-        for modbus_register in MODBUS_REGISTERS:
+        if self._update_list.__len__() != 0:
+            (unique_id, value) = self._update_list.popitem()
+            address = self._address_by_uniqe_id(unique_id)
+            try:
+                self._modbus_client.write_registers(address, value)
+                self._data[unique_id] = value
+            except Exception as e:
+                self.logger.error(f"Error writing '{unique_id}', register {address}.")
+        else:
+            modbus_register = self._next_register()
             address = modbus_register.get("address")
             unique_id = modbus_register.get("unique_id")
             length = modbus_register.get("length")
             scale = modbus_register.get("scale", 1)
-            if unique_id in processed_updates:
-                data[unique_id] = update_cache.get(unique_id)
-            else:
-                try:
-                    data[unique_id] = self._modbus_client.read_holding_registers(address, length).registers[length - 1] * scale
-                except Exception as e:
-                    unique_id = modbus_register.get("unique_id")
-                    address = modbus_register.get("address")
-                    data[modbus_register.get("unique_id")] = None
-        return data
+            try:
+                self._data[unique_id] = self._modbus_client.read_holding_registers(address, length).registers[length - 1] * scale
+            except Exception as e:
+                unique_id = modbus_register.get("unique_id")
+                address = modbus_register.get("address")
+                self._data[modbus_register.get("unique_id")] = None
+                self.logger.error(f"Error reading '{unique_id}', register {address}.")
+        return self._data.copy()
 
-    def write(self, unique_id: str, value: int):
-        self._update_cache[unique_id] = value
-
-    def _process_updates(self):
-        processed_updates = []
-        if self._modbus_client.connect() == True:
-            for (unique_id, value) in self._update_cache.items():
-                address = self._address_by_uniqe_id(unique_id)
-                try:
-                    self._modbus_client.write_registers(address, value)
-                    processed_updates.append(unique_id)
-                except Exception as e:
-                    _LOGGER.log(f"Error writing value for '{unique_id}': {e}")
-            self._update_cache.clear()
-        return processed_updates
+    def _next_register(self):
+        self._current_register = (self._current_register + 1) % MODBUS_REGISTERS.__len__()
+        return MODBUS_REGISTERS[self._current_register]
 
     def _address_by_uniqe_id(self, unique_id: str) -> int:
         for modbus_register in MODBUS_REGISTERS:
